50ecebfbbd0e2c36e699ae336bb94be28bb66cc4
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-03-14 20:15:29 +0900

    Fin ruby_score.rb

--- ruby_money.rb
+++ ruby_money.rb
@@ -3,6 +3,18 @@ money = 6498
 # 紙幣、硬貨は[1, 5, 10, 50, 100, 500, 1000, 5000, 10000]を対象とする
 # {1=>3, 5=>1, 10=>4, 50=>1, 100=>4, 500=>0, 1000=>1, 5000=>1, 10000=>0}
 
-cb = [1,5,10,100,500,1000,5000,10000]
-cb.sort.reverse
+cb = [1,5,10,50,100,500,1000,5000,10000]
+cb.sort!.reverse!
 p cb
+
+r = {}
+cb.each do |n|
+  mai = money / n
+  add_hash = { n => mai}
+  r.merge!(add_hash)
+  unless mai == 0
+    money = money - (n * mai)
+  end
+end
+p money
+p r
